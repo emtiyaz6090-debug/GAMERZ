24a773b44c0fa52a0e083626e1fa56e9bca0f0fc
emtiyaz6090-debug <emtiyaz6090@gmail.com>
2026-04-24 09:03:40 +0600

    Create GlassCard widget for glassmorphism effect
    This widget provides a customizable glassmorphism card with optional tap functionality.

---
+++ lib/widgets/glass_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class GlassCard extends StatelessWidget {
+  final Widget child;
+  final EdgeInsets padding;
+  final double? width;
+  final VoidCallback? onTap;
+
+  const GlassCard({
+    super.key,
+    required this.child,
+    this.padding = const EdgeInsets.all(20),
+    this.width,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width,
+        padding: padding,
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.05),
+          borderRadius: BorderRadius.circular(24),
+          border: Border.all(
+            color: Colors.white.withOpacity(0.1),
+            width: 1,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 10,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(24),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
